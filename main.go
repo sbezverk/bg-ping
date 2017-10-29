1c697663229fe01042b27866e400379630b19e6d
Serguei Bezverkhi <sbezverk@cisco.com>
2017-10-29 18:39:10 -0400

    Refactor to support multiple ping destinations

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net"
 	"os"
 	"os/signal"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
 	"time"
 
@@ -21,11 +21,18 @@ type pingPacket struct {
 	Seq int
 }
 
-var outage bool
+type pClient struct {
+	control chan pingPacket
+	ID      int
+	ip      string
+	outage  bool
+}
+
+var wg sync.WaitGroup
 
 func isValidIPv4(ip string) bool {
 	parts := strings.Split(ip, ".")
-	if len(parts) < 4 {
+	if len(parts) != 4 {
 		return false
 	}
 	for _, x := range parts {
@@ -40,93 +47,14 @@ func isValidIPv4(ip string) bool {
 	return true
 }
 
-func pingServer(c *icmp.PacketConn, targetAddr string, control chan pingPacket, logFile *os.File) {
-	b := make([]byte, 65507)
-	for {
-		count, packetAddr, err := c.ReadFrom(b)
-		if err != nil {
-			recordEvent(fmt.Sprintf("pingServer failed to read icmp packet: %v", err), logFile)
-			continue
-		}
-		if strings.Compare(packetAddr.String(), targetAddr) != 0 {
-			// Receied uninteresting packet, just ignoring it...
-			continue
-		}
-		// log.Printf("pingServer received %d bytes from ip: %s ", count, packetAddr.String())
-		m, err := icmp.ParseMessage(1, b[:count])
-		if err != nil {
-			recordEvent(fmt.Sprintf("pingServer failed to parse icmp packet: %v", err), logFile)
-			continue
-		}
-		switch b := m.Body.(type) {
-		case *icmp.Echo:
-			// log.Printf("Echo reply packet: ID %d Seq %d", b.ID, b.Seq)
-			control <- pingPacket{
-				ID:  b.ID,
-				Seq: b.Seq,
-			}
-		}
-	}
-}
-
 func timeStamp() string {
 	t := time.Now()
 	return fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d_%04d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond()/1000000)
 }
 
-func recordEvent(msg string, logFile *os.File) {
-	r := fmt.Sprintf("| %-30s| %-26s|\n", msg, timeStamp())
-	if _, err := logFile.WriteString(r); err != nil {
-		log.Fatalf("Failed to record event into the log: %v\n", err)
-		os.Exit(1)
-	}
-	logFile.Sync()
-}
-
-func pingClient(c *icmp.PacketConn, targetAddr string, control chan pingPacket, logFile *os.File) {
-
-	processID := rand.Intn(65535)
-	processSeq := 1
-	for {
-		wm := icmp.Message{
-			Type: ipv4.ICMPTypeEcho,
-			Code: 8,
-			Body: &icmp.Echo{
-				ID:   processID,
-				Seq:  processSeq,
-				Data: []byte("12345677890"),
-			},
-		}
-		wb, err := wm.Marshal(nil)
-		if err != nil {
-			recordEvent(fmt.Sprintf("pingClient failed to marshal icmp packet: %v\n", err), logFile)
-		}
-		_, err = c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(targetAddr)})
-		if err != nil {
-			recordEvent(fmt.Sprintf("pingClient failed to send a packet: %v\n", err), logFile)
-		}
-		select {
-		case p := <-control:
-			if p.ID == processID && p.Seq == processSeq {
-				if outage {
-					recordEvent("Connectivity outage cleared", logFile)
-				}
-				outage = false
-				processSeq++
-				time.Sleep(900 * time.Millisecond)
-			}
-		case <-time.After(1900 * time.Millisecond):
-			if !outage {
-				recordEvent("Connectivity outage detected", logFile)
-			}
-			outage = true
-		}
-	}
-}
-
-func startLogging(ip string) *os.File {
+func startLogging() *os.File {
 	t := time.Now()
-	logFileName := fmt.Sprintf("/tmp/bg-ping_%s_%d-%02d-%02dT%02d:%02d:%02d", ip, t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	logFileName := fmt.Sprintf("/tmp/bg-ping_%d-%02d-%02dT%02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 	logFile, err := os.Create(logFileName)
 	if err != nil {
 		log.Fatalf("%s failed to create log %s: %v\n", os.Args[0], logFileName, err)
@@ -135,22 +63,130 @@ func startLogging(ip string) *os.File {
 	return logFile
 }
 
+func parseIPs(params []string) ([]string, error) {
+	var pingIPs []string
+	for _, l1 := range params {
+		l2 := strings.Split(l1, ",")
+		for _, l3 := range l2 {
+			if !isValidIPv4(l3) {
+				return nil, fmt.Errorf(" %s is an invalid ip address", l3)
+			}
+			pingIPs = append(pingIPs, l3)
+		}
+	}
+	return pingIPs, nil
+}
+
+func recordEvent(msg string, logFile *os.File) {
+	r := fmt.Sprintf("| %-80s| %-26s|\n", msg, timeStamp())
+	if _, err := logFile.WriteString(r); err != nil {
+		log.Fatalf("Failed to record event into the log: %v\n", err)
+		os.Exit(1)
+	}
+	logFile.Sync()
+}
+
+func pingServer(c *icmp.PacketConn, clients map[int]pClient, logFile *os.File) {
+	b := make([]byte, 65507)
+	for {
+		count, _, err := c.ReadFrom(b)
+		if err != nil {
+			recordEvent(fmt.Sprintf("pingServer failed to read icmp packet: %v", err), logFile)
+			continue
+		}
+
+		// log.Printf("pingServer received %d bytes from ip: %s ", count, packetAddr.String())
+		m, err := icmp.ParseMessage(1, b[:count])
+		if err != nil {
+			recordEvent(fmt.Sprintf("pingServer failed to parse icmp packet: %v", err), logFile)
+			continue
+		}
+		switch b := m.Body.(type) {
+		case *icmp.Echo:
+			if _, ok := clients[b.ID]; ok {
+				// log.Printf("Sending to client ID: %d channel: %v\n", b.ID, clients[b.ID].control)
+				clients[b.ID].control <- pingPacket{
+					ID:  b.ID,
+					Seq: b.Seq,
+				}
+			}
+		}
+	}
+}
+
+func pingClient(c *icmp.PacketConn, clientID int, client pClient, logFile *os.File) {
+	processSeq := 1
+	for {
+		wm := icmp.Message{
+			Type: ipv4.ICMPTypeEcho,
+			Code: 8,
+			Body: &icmp.Echo{
+				ID:   clientID,
+				Seq:  processSeq,
+				Data: []byte("12345677890"),
+			},
+		}
+		wb, err := wm.Marshal(nil)
+		if err != nil {
+			recordEvent(fmt.Sprintf("pingClient: failed to marshal icmp packet to: %s with: %v", client.ip, err), logFile)
+		}
+		_, err = c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(client.ip)})
+		if err != nil {
+			recordEvent(fmt.Sprintf("pingClient: failed to send a packet to: %s %v", client.ip, err), logFile)
+		}
+		select {
+		case p := <-client.control:
+			if p.ID == clientID {
+				if client.outage {
+					recordEvent(fmt.Sprintf("pingClient: Connectivity outage cleared for: %s", client.ip), logFile)
+				}
+				client.outage = false
+			}
+		case <-time.After(1900 * time.Millisecond):
+			if !client.outage {
+				recordEvent(fmt.Sprintf("pingClient: Connectivity outage detected for: %s", client.ip), logFile)
+			}
+			client.outage = true
+		}
+		processSeq++
+		time.Sleep(900 * time.Millisecond)
+	}
+}
+
 func main() {
 
 	if len(os.Args) < 2 {
-		log.Fatalf("%s missing remote ip address for ping, exiting...", os.Args[0])
+		log.Fatalf("%s missing remote ip address(es) for ping, terminating...", os.Args[0])
 		os.Exit(1)
 	}
-	if !isValidIPv4(os.Args[1]) {
-		log.Fatalf("%s invalid %s is an invalid ip address, exiting...", os.Args[0], os.Args[1])
+
+	// Parse and validate the list of IPs passed as argument(s)
+	pingIPs, err := parseIPs(os.Args[1:])
+	if err != nil {
+		log.Fatalf("%s failed: %v, terminating...", os.Args[0], err)
 		os.Exit(1)
 	}
-	logFile := startLogging(os.Args[1])
+
+	// Start logging
+	logFile := startLogging()
 	defer logFile.Close()
 
+	// Build pingClientsList
+	pingClientList := map[int]pClient{}
+	for id, ip := range pingIPs {
+		pingClientList[id+1] = pClient{
+			control: make(chan pingPacket),
+			ID:      id + 1,
+			ip:      ip,
+			outage:  false,
+		}
+	}
+
+	// Open connection for listen all incoming icmp packets
 	connection, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
-		log.Fatalf("%s failed to listen for icmp packets with: %v, exiting...", os.Args[0], err)
+		recordEvent(fmt.Sprintf("%s failed to listen for icmp packets with: %v, terminating", os.Args[0], err), logFile)
+		os.Exit(1)
 	}
 	defer connection.Close()
 
@@ -163,16 +199,24 @@ func main() {
 		syscall.SIGQUIT)
 	go func() {
 		for sig := range c {
-			fmt.Fprintf(logFile, "%s: Captured %v, closing log and terminating...\n", timeStamp(), sig)
+			recordEvent(fmt.Sprintf("Captured %v, closing log and terminating", sig), logFile)
 			connection.Close()
 			logFile.Close()
 			os.Exit(0)
 		}
 	}()
 
-	fmt.Fprintf(logFile, "%s: Starting pingServer and pingClient checking connectivity to %s .\n", timeStamp(), os.Args[1])
-	outage = false
-	control := make(chan pingPacket)
-	go pingServer(connection, os.Args[1], control, logFile)
-	pingClient(connection, os.Args[1], control, logFile)
+	recordEvent(fmt.Sprintf("Starting pingServer and pingClient"), logFile)
+
+	// Starting pingServer and passing list of all ping clients with their
+	// corresponding information
+	go pingServer(connection, pingClientList, logFile)
+
+	// Adding wait groups just for main to wait on something other than dead loop
+	// this programm does not have a way to terminate other than kill.
+	wg.Add(len(pingClientList))
+	for id := range pingClientList {
+		go pingClient(connection, id, pingClientList[id], logFile)
+	}
+	wg.Wait()
 }
